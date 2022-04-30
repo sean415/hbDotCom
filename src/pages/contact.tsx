f64a5ff81e4430e1f581504efd32327ebdb80191
Sean Slinsky <sean@seanslinsky.com>
2022-04-30 10:49:52 -0700

    use bodycontent component in contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/contact.module.sass';
-import { useForm, ValidationError } from '@formspree/react'
 import ContactForm from '../components/ContactForm/contact-form';
+import { BodyContent } from '../components/BodyContent/body-content';
 
 export default function Contact() {
   return (
@@ -11,10 +10,10 @@ export default function Contact() {
         <title>Holly Burns - Contact</title>
       </Head>
       <div className={styles.contactPage}>
-        <section>
-          <h2>Contact Me</h2>
+        <BodyContent>
+          <h1>Contact Me</h1>
           <p>You can email me at hollyburnswritesATgmail.com or find me on LinkedIn. I am also on Twitter, though it just kind of feels like a lot of people shouting at once.</p>
-        </section>
+        </BodyContent>
         <div className={styles.contactForm}>
           <ContactForm />
         </div>
